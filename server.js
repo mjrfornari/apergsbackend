e2538c5d05d411f73f9955fb11124594b190bc0c
mjrfornari <mjrfornari@gmail.com>
2018-12-20 18:40:13 -0200

    asd
    asd

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+﻿
 var mysql      = require('mysql');
 // var connection = mysql.createConnection({
 //   host     : 'localhost',
@@ -9,7 +9,7 @@ var mysql      = require('mysql');
 
 //mysql://b2a54ba1936954:69dd8472@us-cdbr-iron-east-01.cleardb.net/heroku_bc855f521588179?reconnect=true
 
-var db_config = mysql.createConnection({
+var connection = mysql.createConnection({
   host     : 'us-cdbr-iron-east-01.cleardb.net',
   user     : 'b2a54ba1936954',
   password : '69dd8472',
@@ -18,30 +18,7 @@ var db_config = mysql.createConnection({
 
 
 
-var connection;
 
-function handleDisconnect() {
-  connection = mysql.createConnection(db_config); // Recreate the connection, since
-                                                  // the old one cannot be reused.
-
-  connection.connect(function(err) {              // The server is either down
-    if(err) {                                     // or restarting (takes a while sometimes).
-      console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-    }                                     // to avoid a hot loop, and to allow our node script to
-  });                                     // process asynchronous requests in the meantime.
-                                          // If you're also serving http, display a 503 error.
-  connection.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleDisconnect();                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      throw err;                                  // server variable configures this)
-    }
-  });
-}
-
-handleDisconnect();
 
 let app = require('express')()
 let http = require('http').Server(app)
