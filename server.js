b5c419092a4c5c37f57987e8a07a80f1557923bb
mjrfornari <mjrfornari@gmail.com>
2018-12-27 09:21:44 -0200

    asd
    asd

--- server.js
+++ server.js
@@ -1,24 +1,48 @@
 ﻿
 var mysql      = require('mysql');
-// var connection = mysql.createConnection({
-//   host     : 'localhost',
-//   user     : 'root',
-//   password : 'eloperdido',
-//   database : 'apergsdb',
-// });
+//var connection = mysql.createConnection({
+//  host     : 'localhost',
+//  user     : 'root',
+//  password : 'eloperdido',
+//  database : 'apergsdb',
+//});
 
 //mysql://b2a54ba1936954:69dd8472@us-cdbr-iron-east-01.cleardb.net/heroku_bc855f521588179?reconnect=true
 
-var connection = mysql.createConnection({
-  host     : 'us-cdbr-iron-east-01.cleardb.net',
-  user     : 'b2a54ba1936954',
-  password : '69dd8472',
-  database : 'heroku_bc855f521588179'
-});
+ var connection = mysql.createPool({
+     connectTimeout : 1000,
+     host     : 'us-cdbr-iron-east-01.cleardb.net',
+     user     : 'b2a54ba1936954',
+     password : '69dd8472',
+     database : 'heroku_bc855f521588179'
+ });
 
 
 
+// var connection;
 
+// function handleDisconnect() {
+//   connection = mysql.createConnection(db_config); // Recreate the connection, since
+//                                                   // the old one cannot be reused.
+
+//   connection.connect(function(err) {              // The server is either down
+//     if(err) {                                     // or restarting (takes a while sometimes).
+//       console.log('error when connecting to db:', err);
+//       setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+//     }                                     // to avoid a hot loop, and to allow our node script to
+//   });                                     // process asynchronous requests in the meantime.
+//                                           // If you're also serving http, display a 503 error.
+//   connection.on('error', function(err) {
+//     console.log('db error', err);
+//     if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+//       handleDisconnect();                         // lost due to either server restart, or a
+//     } else {                                      // connnection idle timeout (the wait_timeout
+//       throw err;                                  // server variable configures this)
+//     }
+//   });
+// }
+
+// handleDisconnect();
 
 let app = require('express')()
 let http = require('http').Server(app)
@@ -68,7 +92,39 @@ app.get('/api/getAssociados',  function (req, res, next) {
 
     connection.query(sql, function (error, results, fields) {
         if (error) throw error;
-        // connection.end();
+        res.json(results)
+    });
+    
+
+})
+
+
+app.get('/api/getPaises',  function (req, res, next) {
+    let sql = 'SELECT * from paises ';
+
+    if (Number(req.query.pk) > 0) {
+        sql = sql + 'where pk_pai = '+(Number(req.query.pk)).toString()
+    }
+
+    connection.query(sql, function (error, results, fields) {
+        if (error) throw error;
+        res.json(results)
+    });
+    
+
+})
+
+
+app.get('/api/getEstados',  function (req, res, next) {
+    let sql = 'SELECT est.*, pai.nome nomepai from estados est '+
+        ' join paises pai on pai.pk_pai = est.fk_pai ';
+
+    if (Number(req.query.pk) > 0) {
+        sql = sql + 'where pk_est = '+(Number(req.query.pk)).toString()
+    }
+
+    connection.query(sql, function (error, results, fields) {
+        if (error) throw error;
         res.json(results)
     });
     
@@ -109,6 +165,57 @@ app.get('/api/getLotacoes',  function (req, res, next) {
 
 app.post('/api/novoAssociado',  function (req, res, next) {
     let sql = 'insert into ASSOCIADOS ('
+    let data = req.body
+    let fields = Object.getOwnPropertyNames(data)
+    let values = [Object.values(data)]
+    sql = sql + fields.toString()+') values ?'
+    connection.query(sql, [values], function (error, results, fields){
+          if(error) {
+              res.status(500).json(error)
+              return console.log(error);
+          }
+          console.log('adicionou registros!');
+          res.status(200).json({message: 'Success!', pk: results.insertId})
+      });
+    
+})
+
+app.post('/api/novoPaises',  function (req, res, next) {
+    let sql = 'insert into PAISES ('
+    let data = req.body
+    let fields = Object.getOwnPropertyNames(data)
+    let values = [Object.values(data)]
+    sql = sql + fields.toString()+') values ?'
+    connection.query(sql, [values], function (error, results, fields){
+          if(error) {
+              res.status(500).json(error)
+              return console.log(error);
+          }
+          console.log('adicionou registros!');
+          res.status(200).json({message: 'Success!', pk: results.insertId})
+      });
+    
+})
+
+app.post('/api/novoEstados',  function (req, res, next) {
+    let sql = 'insert into ESTADOS ('
+    let data = req.body
+    let fields = Object.getOwnPropertyNames(data)
+    let values = [Object.values(data)]
+    sql = sql + fields.toString()+') values ?'
+    connection.query(sql, [values], function (error, results, fields){
+          if(error) {
+              res.status(500).json(error)
+              return console.log(error);
+          }
+          console.log('adicionou registros!');
+          res.status(200).json({message: 'Success!', pk: results.insertId})
+      });
+    
+})
+
+app.post('/api/novoCidades',  function (req, res, next) {
+    let sql = 'insert into CIDADES ('
     let data = req.body
     let fields = Object.getOwnPropertyNames(data)
     let values = [Object.values(data)]
@@ -167,6 +274,104 @@ app.post('/api/editAssociado',  function (req, res, next) {
     }
 })
 
+
+app.post('/api/deleteAssociado',  function (req, res, next) {
+    if (Number(req.query.pk) > 0) {
+        let sql = 'delete from ASSOCIADOS where pk_ass=? '
+        let pk = Number(req.query.pk)
+        connection.query(sql, pk, function (error, results, fields){
+            if(error) {
+                res.status(500).json(error)
+                return console.log(error);
+            }
+            // console.log(mysql.format(sql, pk))
+            console.log('excluiu registro!');
+            console.log(results)
+            res.status(200).json({message: 'Success!', rows: results.affectedRows})
+        });
+    } else {
+        res.status(500).json({ error: 'Código do registro não encontrado!'})
+    }
+})
+
+app.post('/api/deleteCelular',  function (req, res, next) {
+    if (Number(req.query.pk) > 0) {
+        let sql = 'delete from ASSOCIADOS_CELULARES where pk_ace=? '
+        let pk = Number(req.query.pk)
+        connection.query(sql, pk, function (error, results, fields){
+            if(error) {
+                res.status(500).json(error)
+                return console.log(error);
+            }
+            // console.log(mysql.format(sql, pk))
+            console.log('excluiu registro!');
+            console.log(results)
+            res.status(200).json({message: 'Success!', rows: results.affectedRows})
+        });
+    } else {
+        res.status(500).json({ error: 'Código do registro não encontrado!'})
+    }
+})
+
+
+app.post('/api/deletePaises',  function (req, res, next) {
+    if (Number(req.query.pk) > 0) {
+        let sql = 'delete from PAISES where pk_pai=? '
+        let pk = Number(req.query.pk)
+        connection.query(sql, pk, function (error, results, fields){
+            if(error) {
+                res.status(500).json(error)
+                return console.log(error);
+            }
+            // console.log(mysql.format(sql, pk))
+            console.log('excluiu registro!');
+            console.log(results)
+            res.status(200).json({message: 'Success!', rows: results.affectedRows})
+        });
+    } else {
+        res.status(500).json({ error: 'Código do registro não encontrado!'})
+    }
+})
+
+
+app.post('/api/deleteEstados',  function (req, res, next) {
+    if (Number(req.query.pk) > 0) {
+        let sql = 'delete from ESTADOS where pk_est=? '
+        let pk = Number(req.query.pk)
+        connection.query(sql, pk, function (error, results, fields){
+            if(error) {
+                res.status(500).json(error)
+                return console.log(error);
+            }
+            // console.log(mysql.format(sql, pk))
+            console.log('excluiu registro!');
+            console.log(results)
+            res.status(200).json({message: 'Success!', rows: results.affectedRows})
+        });
+    } else {
+        res.status(500).json({ error: 'Código do registro não encontrado!'})
+    }
+})
+
+app.post('/api/deleteCidades',  function (req, res, next) {
+    if (Number(req.query.pk) > 0) {
+        let sql = 'delete from CIDADES where pk_cid=? '
+        let pk = Number(req.query.pk)
+        connection.query(sql, pk, function (error, results, fields){
+            if(error) {
+                res.status(500).json(error)
+                return console.log(error);
+            }
+            // console.log(mysql.format(sql, pk))
+            console.log('excluiu registro!');
+            console.log(results)
+            res.status(200).json({message: 'Success!', rows: results.affectedRows})
+        });
+    } else {
+        res.status(500).json({ error: 'Código do registro não encontrado!'})
+    }
+})
+
 app.post('/api/editCelular',  function (req, res, next) {
     if (Number(req.query.pk) > 0) {
         let sql = 'update ASSOCIADOS_CELULARES set '
@@ -194,8 +399,85 @@ app.post('/api/editCelular',  function (req, res, next) {
 
 
 
+app.post('/api/editPaises',  function (req, res, next) {
+    if (Number(req.query.pk) > 0) {
+        let sql = 'update PAISES set '
+        let pk = Number(req.query.pk)
+        let data = req.body
+        let fields = Object.getOwnPropertyNames(data)
+        let teste = fields.join('=?, ')+'=?'
+        let values = Object.values(data)
+        let fieldsnvalues = mysql.format(teste, values)
+        sql = sql + fieldsnvalues + ' where pk_pai=?'
+        connection.query(sql, pk, function (error, results, fields){
+            if(error) {
+                res.status(500).json(error)
+                return console.log(error);
+            }
+            // console.log(mysql.format(sql, pk))
+            console.log('alterou registros!');
+            console.log(results)
+            res.status(200).json({message: 'Success!', rows: results.affectedRows})
+        });
+    } else {
+        res.status(500).json({ error: 'Código do registro não encontrado!'})
+    }
+})
+
+app.post('/api/editEstados',  function (req, res, next) {
+    if (Number(req.query.pk) > 0) {
+        let sql = 'update ESTADOS set '
+        let pk = Number(req.query.pk)
+        let data = req.body
+        let fields = Object.getOwnPropertyNames(data)
+        let teste = fields.join('=?, ')+'=?'
+        let values = Object.values(data)
+        let fieldsnvalues = mysql.format(teste, values)
+        sql = sql + fieldsnvalues + ' where pk_est=?'
+        connection.query(sql, pk, function (error, results, fields){
+            if(error) {
+                res.status(500).json(error)
+                return console.log(error);
+            }
+            // console.log(mysql.format(sql, pk))
+            console.log('alterou registros!');
+            console.log(results)
+            res.status(200).json({message: 'Success!', rows: results.affectedRows})
+        });
+    } else {
+        res.status(500).json({ error: 'Código do registro não encontrado!'})
+    }
+})
+
+app.post('/api/editCidades',  function (req, res, next) {
+    if (Number(req.query.pk) > 0) {
+        let sql = 'update CIDADES set '
+        let pk = Number(req.query.pk)
+        let data = req.body
+        let fields = Object.getOwnPropertyNames(data)
+        let teste = fields.join('=?, ')+'=?'
+        let values = Object.values(data)
+        let fieldsnvalues = mysql.format(teste, values)
+        sql = sql + fieldsnvalues + ' where pk_cid=?'
+        connection.query(sql, pk, function (error, results, fields){
+            if(error) {
+                res.status(500).json(error)
+                return console.log(error);
+            }
+            // console.log(mysql.format(sql, pk))
+            console.log('alterou registros!');
+            console.log(results)
+            res.status(200).json({message: 'Success!', rows: results.affectedRows})
+        });
+    } else {
+        res.status(500).json({ error: 'Código do registro não encontrado!'})
+    }
+})
+
+
+
 app.get('/api/getCidades',  function (req, res, next) {
-    let sql = 'SELECT cid.*, est.sigla, est.nome, pai.nome, concat(cid.nome," - ",est.sigla) as descricao from cidades cid '+
+    let sql = 'SELECT cid.*, est.sigla, est.nome nomeest, pai.nome nomepai, est.fk_pai, concat(cid.nome," - ",est.sigla) as descricao from cidades cid '+
     ' join estados est on est.pk_est = cid.fk_est '+
     ' join paises pai on pai.pk_pai = est.fk_pai '
 
